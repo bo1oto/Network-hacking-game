255e334fe14344a23649408a03ece7fd1d62045b
Vladislav <64910163+bo1oto@users.noreply.github.com>
2021-07-13 21:27:33 +0300

    Update NodeSC.h

--- Code/Nodes/NodeSC.h
+++ Code/Nodes/NodeSC.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -9,9 +8,6 @@
 #include "NodeBase.h"
 #include "NodeSC.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class UNCRUSHABLE_API ANodeSC : public ANodeBase
 {
