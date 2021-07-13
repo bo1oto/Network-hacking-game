2008e0f86dd52010483f677a783584a9f64c2446
Vladislav <64910163+bo1oto@users.noreply.github.com>
2021-07-13 21:29:17 +0300

    Update NodeTI.h

--- Code/Nodes/NodeTI.h
+++ Code/Nodes/NodeTI.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,9 +5,6 @@
 #include "NodeBase.h"
 #include "NodeTI.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class UNCRUSHABLE_API ANodeTI : public ANodeBase
 {
@@ -26,13 +22,6 @@ private:
 
 	void AcceptPacket(APacket* packet) final;
 
-
-	//Бред
-	//Маршрутизатор также как и другие ноды знает своих соседей
-	//Но его фишка в том, что он знает в каком vlan находятся другие ноды (ну или какие ноды у vlan соседнего маршрутизатора)
-	//Соответственно мне нужно хранить записи в виде: vlan, id[]
-	//Тогда vlan выдаётся обычным узлам при подключении к TI, а если подключение идёт из обычного к обычному, то тому присваивается vlan другого
-	//Также при подключении к TI обычного узла, его соединения также обновляют vlan
 	struct Routing final
 	{
 		Routing(int vlan_num, std::vector<int> vec = {});
@@ -40,13 +29,8 @@ private:
 		int vlan;
 		std::vector<int> id_numbers;
 	};
-	//Нулевой - это собственный vlan
 	std::vector<Routing*> routingTable;
 	
-	//Нужен метод, который переберёт все узлы в границах этого маршрутизатора
-	//Но мне это пока не нужно, я могу по сути просто рукой назначать vlan, буду рукой вводить
-	
-	//я в блупринтах нахожу все ноды этого типа, и потом для каждого вызываю метод с каждым
 	UFUNCTION(BlueprintCallable)
 	void CreateVLAN(ANodeTI* node);
 	void FillVLAN(std::vector<int>* id_vec, int vlan_num);
