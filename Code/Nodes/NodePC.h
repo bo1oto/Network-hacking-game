a3ee3fce918d9d434527ee71d6e386885621a970
Vladislav <64910163+bo1oto@users.noreply.github.com>
2021-07-13 21:23:53 +0300

    Update NodePC.h

--- Code/Nodes/NodePC.h
+++ Code/Nodes/NodePC.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,9 +5,6 @@
 #include "NodeBase.h"
 #include "NodePC.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class UNCRUSHABLE_API ANodePC : public ANodeBase
 {
