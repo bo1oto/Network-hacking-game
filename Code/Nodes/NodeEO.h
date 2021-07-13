2ff5befdb9931415c6e322c8908a966b0810cca3
Vladislav <64910163+bo1oto@users.noreply.github.com>
2021-07-13 21:20:43 +0300

    Update NodeEO.h

--- Code/Nodes/NodeEO.h
+++ Code/Nodes/NodeEO.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,9 +5,6 @@
 #include "NodeBase.h"
 #include "NodeEO.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class UNCRUSHABLE_API ANodeEO : public ANodeBase
 {
@@ -17,12 +13,9 @@ class UNCRUSHABLE_API ANodeEO : public ANodeBase
 public:
 	ANodeEO();
 	void BeginPlay() final;
-	//void Tick(float DeltaTime) override final;
 
 	static int id_counter;//EO - 0-5
 private:
-	/*void CheckPacket(APacket* packet, std::vector<ANodeBase*>* vec, std::vector<ANodeBase*>::iterator it, float time = 0.0f) override final;
-	void GeneratePacket(int chance) override final;*/
 	void AcceptPacket(APacket* packet) final;
 	
 };
