63615c48a59d22b672a8a1ca958054dd055303f7
Vladislav <64910163+bo1oto@users.noreply.github.com>
2021-08-19 14:19:41 +0300

    Update NodePC.h

--- Code/Nodes/NodePC.h
+++ Code/Nodes/NodePC.h
@@ -13,13 +13,13 @@ class UNCRUSHABLE_API ANodePC : public ANodeBase
 public:
 	ANodePC();
 	void BeginPlay() final;
-	void Tick(float DeltaTime) final;
 	UPROPERTY(BlueprintReadWrite, Category = "GameRules")
 	bool have_phys_connection = false;
 
 	static int id_counter;//PC - 70-89
+
+	void GeneratePacket(int chance) final;
 private:
 
 	void AcceptPacket(APacket* packet) final;
-	void GeneratePacket(int chance) final;
 };
