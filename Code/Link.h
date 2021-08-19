86544f6a228debe0db4f249ecebea49e5e23f7fb
Vladislav <64910163+bo1oto@users.noreply.github.com>
2021-08-19 13:45:56 +0300

    Update Link.h

--- Code/Link.h
+++ Code/Link.h
@@ -26,8 +26,22 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 
-	bool isAlive;
+	UFUNCTION(BlueprintCallable)
+	TArray<FText> GetKeyParameters();
+	FText GetTypeInfo();
+	UPROPERTY(BlueprintReadWrite)
+	bool isAlive = true;
+	UPROPERTY(BlueprintReadWrite)
+	int max_length;
 	LinkType linkType;
+
+	float speed_coef;
+	short current_load = 0;
+	short max_load;
+	short interference_immunity;
+
+	inline void AddWorkload(short quantity, float last_packet_time);
+	void AddWorkloadWithDelay(short _add_work, float delay_time);
 
 	UFUNCTION(BlueprintCallable)
 	void SetLinkType(int num);
