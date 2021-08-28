88c4e7d2f49af6b05a2ad42df16e62f6209b4ec6
Vladislav <64910163+bo1oto@users.noreply.github.com>
2021-08-28 13:06:41 +0300

    Update NodeTI.cpp

--- Code/Nodes/NodeTI.cpp
+++ Code/Nodes/NodeTI.cpp
@@ -24,7 +24,7 @@ void ANodeTI::BeginPlay()
 	vlan_counter++;
 	sInformation = new Information;
 }
-
+		
 void ANodeTI::AcceptPacket(APacket* packet)
 {
 	if (packet->target_id != id)
@@ -43,7 +43,7 @@ void ANodeTI::AcceptPacket(APacket* packet)
 				}
 			}
 			std::vector<ANodeBase*>* nodes = new std::vector<ANodeBase*>;
-			for (std::vector<ANodeBase*>::reverse_iterator it = bolv.rbegin(); it != bolv.rend(); it++)
+			for (auto it = bolv.rbegin(); it != bolv.rend(); it++)
 			{
 				nodes->push_back(*it);
 			}
@@ -58,7 +58,7 @@ void ANodeTI::AcceptPacket(APacket* packet)
 		{
 			std::vector<ANodeBase*>* nodes = new std::vector<ANodeBase*>;
 			std::vector<ANodeBase*> bolv = ComputeNodePath(this, packet->target_id);
-			for (std::vector<ANodeBase*>::reverse_iterator it = bolv.rbegin(); it != bolv.rend(); it++)
+			for (auto it = bolv.rbegin(); it != bolv.rend(); it++)
 			{
 				nodes->push_back(*it);
 			}
@@ -82,11 +82,11 @@ void ANodeTI::AcceptPacket(APacket* packet)
 	}
 	
 }
-bool ANodeTI::CheckIDInTable(int _id)
+bool ANodeTI::CheckIDInTable(int _id) const
 {
-	auto contain = [](std::vector<int>* id_vec, int node_id) -> bool
+	auto contain = [](const std::vector<int>& id_vec, int node_id) -> bool
 	{
-		for (auto elem : (*id_vec))
+		for (auto elem : id_vec)
 		{
 			if (elem == node_id) return true;
 		}
@@ -94,12 +94,12 @@ bool ANodeTI::CheckIDInTable(int _id)
 	};
 	for (auto routes : routingTable)
 	{
-		if (contain(&(routes->id_numbers), _id)) return true;
+		if (contain(routes->id_numbers, _id)) return true;
 	}
 	return false;
 }
 
-ANodeTI::Routing::Routing(int vlan_num, std::vector<int> vec)
+ANodeTI::Routing::Routing(int vlan_num)
 {
 	vlan = vlan_num;
 	id_numbers = {};
@@ -109,39 +109,39 @@ void ANodeTI::CreateVLAN(ANodeTI* node)
 {
 	Routing* route = new Routing(node->vlan);
 	routingTable.push_back(route);
-	FillVLAN(&(route->id_numbers), route->vlan);
+	FillVLAN(route->id_numbers, route->vlan);
 }
-void ANodeTI::FillVLAN(std::vector<int>* id_vec, int vlan_num)
+void ANodeTI::FillVLAN(std::vector<int>& id_vec, int vlan_num)
 {
-	id_vec->clear();
-	auto contain = [](std::vector<int>* id_vec, int node_id) -> bool
+	auto contain = [](const std::vector<int>& id_vec, int node_id) -> bool
 	{
-		for (auto elem : (*id_vec))
+		for (auto elem : id_vec)
 		{
-			if (elem == node_id)
-				return true;
+			if (elem == node_id) return true;
 		}
 		return false;
 	};
-	std::function<void(std::vector<int>* id_vec, ANodeBase* target)> f = 
-		[&f, vlan_num, &contain](std::vector<int>* id_vec, ANodeBase* target) -> void
+
+	std::function<void(std::vector<int>& id_vec, ANodeBase* target)> f = 
+		[&f, vlan_num, &contain](std::vector<int>& id_vec, ANodeBase* target) -> void
 	{
 		for (auto nodeLink : target->nodeLinks)
 		{
 			if (nodeLink->targetNode->vlan == vlan_num && !contain(id_vec, nodeLink->targetNode->id))
 			{
-				id_vec->push_back(nodeLink->targetNode->id);
+				id_vec.push_back(nodeLink->targetNode->id);
 				f(id_vec, nodeLink->targetNode);
 			}
 		}
 	};
+
 	f(id_vec, this);
-	if (vlan == vlan_num && !contain(id_vec, id)) id_vec->push_back(id);
-	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, (TEXT("VLAN ") + FString::FromInt(vlan) + TEXT(" members: ") + FString::FromInt(id_vec->size())));
+	if (vlan == vlan_num && !contain(id_vec, id)) id_vec.push_back(id);
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, (TEXT("VLAN ") + FString::FromInt(vlan) + TEXT(" members: ") + FString::FromInt(id_vec.size())));
 
 	TArray<AActor*> nodes_arr = TArray<AActor*>();
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ANodeBase::StaticClass(), nodes_arr);
-	nodes_arr = nodes_arr.FilterByPredicate( [id_vec, contain](AActor *const &node_ptr)
+	nodes_arr = nodes_arr.FilterByPredicate( [id_vec, contain](AActor* const &node_ptr)
 	{
 		return contain(id_vec, ((ANodeBase*)node_ptr)->id);
 	});
