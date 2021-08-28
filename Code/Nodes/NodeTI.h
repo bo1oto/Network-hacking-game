f81b5d4e676c34d3eea7e6f93a92ee634ee8b9c5
Vladislav <64910163+bo1oto@users.noreply.github.com>
2021-08-28 12:47:09 +0300

    Update NodeTI.h

--- Code/Nodes/NodeTI.h
+++ Code/Nodes/NodeTI.h
@@ -10,22 +10,11 @@ class UNCRUSHABLE_API ANodeTI : public ANodeBase
 {
 	GENERATED_BODY()
 
-public:
-	ANodeTI();
-	void BeginPlay() final;
-
-	bool CheckIDInTable(int _id);
-
-	static int id_counter;//TI - 10-29
-private:
 	static int vlan_counter;
-
-	void AcceptPacket(APacket* packet) final;
 
 	struct Routing final
 	{
 		Routing(int vlan_num, std::vector<int> vec = {});
-	public:
 		int vlan;
 		std::vector<int> id_numbers;
 	};
@@ -34,4 +23,13 @@ private:
 	UFUNCTION(BlueprintCallable)
 	void CreateVLAN(ANodeTI* node);
 	void FillVLAN(std::vector<int>* id_vec, int vlan_num);
+	
+	void AcceptPacket(APacket* packet) final;
+public:
+	ANodeTI();
+	void BeginPlay() final;
+
+	bool CheckIDInTable(int _id) const;
+
+	static int id_counter;//TI - 10-29
 };
